f7a06baa5289f5ddb2e1812ea551680f3c8d9d46
vikas ranjan <vikasranjan786.vr@gmail.com>
2022-10-07 22:35:19 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,62 @@
+
+import React from "react";
+import { useEffect,useState } from "react";
+import './App.css'
+import SearchIcon from './search.svg'
+import Movie from "./Movie";
+
+
+const REACT_APP_OMDB = 'https://www.omdbapi.com/?i=tt3896198&apikey=25f29436'
+
+const App = () => {
+
+
+  const [movies,setMovies] = useState([]);
+  const [searchTerm,setSearchTerm] = useState([]);
+
+  const serachMovies = async (title) =>{
+
+    const result = await fetch(`${REACT_APP_OMDB}&s=${title}`);  
+    const data = await result.json();
+    setMovies(data.Search);
+
+
+  }
+
+  useEffect( () => {
+  
+    serachMovies('Avengers')
+
+  },[])
+  
+  return (
+    <div className="app">
+      <h1>MovieBox</h1>
+      
+      <div className="search">
+        <input placeholder="search for movie" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+        <img src={SearchIcon} alt="search" onClick={ ()=> serachMovies(searchTerm) } />
+      </div>
+
+      {
+        movies?.length > 0
+        
+        ?( <div className="container">
+            { movies.map((movie) => {
+               return  <Movie movie={movie} />         
+            })
+          } 
+          </div>
+        ) :(
+           <div className="empty">
+             <h2>No movie found</h2>
+             </div>
+        )
+      }
+      
+      
+    </div>
+  );
+}
+
+export default App;
